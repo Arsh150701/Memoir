d4a9b969dd72f3bbc02d923147f747c2ae63d935
Arsh150701 <45517849+Arsh150701@users.noreply.github.com>
2021-06-06 21:01:34 +0530

    implemented searchbar

--- src/assets/data/PoemsData.js
+++ src/assets/data/PoemsData.js
@@ -51,7 +51,7 @@ But my hands,
 they move on their own.
 Defying logic and sense;
 Expressing, me.`,
-    tags: ['anxiety', 'depression', 'self-worth'],
+    tags: ['anxiety', 'depression'],
     date: '',
   },
   {
@@ -85,6 +85,131 @@ Not yet, not yet, not yet`,
     tags: ['depression'],
     date: '',
   },
+  {
+    id: 2,
+    title: 'Dilemma',
+    body: `I don't know if it makes sense
+But somedays I'm so full
+That on the days I'm not
+I feel hollow
+
+Don't get me wrong
+I do appreciate the highs
+But these damned highs
+They are followed by withdrawal
+
+I am not used to be radiant
+That's the reason of me being skeptic
+You have that effect on me
+That makes me carefree
+
+I want you for me
+More so that you can save me
+But when you wear off
+There's just me and it scares me`,
+    tags: ['Love', 'Nostalgia'],
+    date: '',
+  },
+  {
+    id: 3,
+    title: 'GHOSTS',
+    body: `It’s 3 A.M. in the morning
+I am smoking by the yellow lamp
+wearing the same old twinning tee
+trying to breathe myself to death
+
+I look around to endless trails of light passing by
+and wonder about destiny and our ratracing lives
+suddenly among the aesthetics came the wind of ghosts
+even scientists love to conspire about things of sorts
+
+They say that spirits are strongest between the hour to pass
+but nothing’s really bothering me other than my past
+They say demons posses our body, diminishes our souls
+and I can’t help but wonder they got it all wrong
+
+The night is calm and our eyes awake, 
+head’s stirring and heart agitates
+We have our demons wailing in us, 
+waiting to scream when we’re worn off
+
+And drifting along it’s just a quarter to four
+and I decide to have a walk
+Gazed upon my eyes the darker roads
+audacious of any demon I walked
+
+At the edge of the yellow shower there were shadows to torch and attention to fill
+but the tenuous mind did none of these
+Entering the noir, there were no doubts
+One foot after the other till I had nocturnal eyes
+
+These ghosts they spoke of didin’t scare me
+because we have our prattling all the time
+and it’s not weak to have a seat in a corner
+because broken are those who are too strong to verbalize `,
+    tags: ['', '', ''],
+    date: '3rd February, 2020',
+  },
+  {
+    id: 4,
+    title: 'GETAWAY',
+    body: `Today I sat with a thought on my head
+wondering about acceptance and pride
+wondering about credibility and life span
+wondering how we perceive things
+
+it occured to me that greatness is not universal
+that legends are not endeared by all
+it’s the degree of relatability that you hold
+the bond that shows you’re only still human
+
+the struggles you go through and the mistakes you make
+the love you lost and ways you escaped
+the mischeivious pranks and punishments you had
+the sleepless nights and following hungover days
+
+you can only ever be so perfect
+that you forget to honour your scars
+to be so well over your past which made you who you are
+that you exceed out of my definition of perfect
+
+to me perfect is one who knows them well
+no Rama required in this era
+just give me a Karan like friend
+
+we are a cursed generation
+shunned by our own biddings
+we all are haunted by our demons
+and just need a goto constant
+
+some are blessed with people to trust
+some are still on their way
+just know that I’m here to lend an ear
+because I too had a shout that stayed in me`,
+    tags: ['', '', ''],
+    date: '24th April, 2020',
+  },
+  // {
+  //   id: 0,
+  //   title: '',
+  //   body: ``,
+  //   tags: ['', '', ''],
+  //   date: '24th April, 2020',
+  // },
+  // {
+  //   id: 0,
+  //   title: '',
+  //   body: ``,
+  //   tags: ['', '', ''],
+  //   date: '',
+  // },
+  // {
+  //   id: 0,
+  //   title: '',
+  //   body: ``,
+  //   tags: ['', '', ''],
+  //   date: '',
+  // },
 ];
 
 export default poemsData;
@@ -93,7 +218,7 @@ export default poemsData;
 // {
 //     id: 0,
 //     title: '',
-//     body: '',
+//     body: ``,
 //     tags: ['', '', ''],
 //     date: '',
 // },
--- src/screens/Poems.js
+++ src/screens/Poems.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   FlatList,
   Pressable,
@@ -8,12 +8,15 @@ import {
   View,
   Image,
 } from 'react-native';
-import DATA from '../assets/data/PoemsData';
+import {SearchBar} from 'react-native-elements';
+import PoemsData from '../assets/data/PoemsData';
 
 export default function Poems() {
+  const [DATA, setDATA] = useState(PoemsData);
   const [collapsed, setCollapsed] = useState(() =>
     DATA.map((item, id) => true),
   );
+  const [search, setSearch] = useState('');
 
   function toggleStates(id) {
     setCollapsed(prev => {
@@ -21,55 +24,73 @@ export default function Poems() {
     });
   }
 
+  useEffect(() => {
+    setDATA(
+      PoemsData.filter(each => {
+        return each.title.toLowerCase().includes(search.toLowerCase()) === true;
+      }),
+    );
+
+    return {DATA};
+  }, [search]);
+
   return (
-    <FlatList
-      style={{marginBottom: 10, paddingHorizontal: 15}}
-      data={DATA}
-      keyExtractor={item => item.id}
-      renderItem={item => (
-        <View>
-          <Text style={styles.title}>{item.item.title}</Text>
-          <ScrollView
-            style={
-              ([{width: '100%'}], collapsed[item.item.id] && {height: 160})
-            }>
-            <Text style={styles.body}>{item.item.body}</Text>
-          </ScrollView>
-          <Pressable
-            style={{
-              flex: 1,
-              flexDirection: 'row',
-              marginTop: 10,
-            }}
-            onPress={() => toggleStates(item.item.id)}>
-            <View
+    <>
+      <SearchBar
+        lightTheme={true}
+        placeholder="Search"
+        onChangeText={e => setSearch(e)}
+        value={search}
+      />
+      <FlatList
+        style={{marginBottom: 10, paddingHorizontal: 15}}
+        data={DATA}
+        keyExtractor={item => item.id}
+        renderItem={item => (
+          <View>
+            <Text style={styles.title}>{item.item.title}</Text>
+            <ScrollView
+              style={
+                ([{width: '100%'}], collapsed[item.item.id] && {height: 160})
+              }>
+              <Text style={styles.body}>{item.item.body}</Text>
+            </ScrollView>
+            <Pressable
               style={{
                 flex: 1,
                 flexDirection: 'row',
-                alignContent: 'flex-start',
-              }}>
-              <Text style={styles.tagTitle}>Tags: </Text>
-              {item.item.tags.map((each, id) => (
-                <Text style={styles.tags}>{each}</Text>
-              ))}
-            </View>
-            <View style={{alignContent: 'flex-end'}}>
-              {collapsed[item.item.id] ? (
-                <Image
-                  style={{height: 30, width: 30}}
-                  source={require('../assets/icons/expand-arrow.png')}
-                />
-              ) : (
-                <Image
-                  style={{height: 30, width: 30}}
-                  source={require('../assets/icons/collapse-arrow.png')}
-                />
-              )}
-            </View>
-          </Pressable>
-        </View>
-      )}
-    />
+                marginTop: 10,
+              }}
+              onPress={() => toggleStates(item.item.id)}>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  alignContent: 'flex-start',
+                }}>
+                <Text style={styles.tagTitle}>Tags: </Text>
+                {item.item.tags.map((each, id) => (
+                  <Text style={styles.tags}>{each}</Text>
+                ))}
+              </View>
+              <View style={{alignContent: 'flex-end'}}>
+                {collapsed[item.item.id] ? (
+                  <Image
+                    style={{height: 30, width: 30}}
+                    source={require('../assets/icons/expand-arrow.png')}
+                  />
+                ) : (
+                  <Image
+                    style={{height: 30, width: 30}}
+                    source={require('../assets/icons/collapse-arrow.png')}
+                  />
+                )}
+              </View>
+            </Pressable>
+          </View>
+        )}
+      />
+    </>
   );
 }
 
